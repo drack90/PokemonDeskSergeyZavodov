8f82596d490ee235abbaa45088075a037ae6c60a
Sergey Zavodov <38080297+drack90@users.noreply.github.com>
2020-11-17 15:10:49 +0500

    Update tets.js

--- tets.js
+++ tets.js
@@ -1,4 +1,4 @@
-<script>
+
 let phone = '';
 
 function formattedPhone(phone){
@@ -7,39 +7,34 @@ let formatedPhone = '';
   let cityCode = '';
   let routeCode ='';
   let numberCode
+  let newPhone
 for(let i=0; i<phone.length; i++){
-  
-//    let formatedPhone = phone.substr(0, 2) + ' (' 
-//                      + phone.substr(2, 3) + ') ' 
-//                      + phone.substr(5, 3) + '-'
-//                      + phone.substr(8, 2) + '-'
-//                      + phone.substr(10, 2);
-    
-  if(phone.charAt(i) == '7'){
-   countryCode = phone.charAt(0) + 
+
+  if(phone.charAt(i) == '7' && i < 2){
+   newPhone = phone.charAt(0) + 
    phone.charAt(1) + ' ';
   } 
-  if(phone.charAt(i) == '1'){
-    cityCode = '(' + phone.charAt(2)+phone.charAt(3) + phone.charAt(4)+ ') ';
+  
+  if(phone.charAt(i) == '1'){    
+    newPhone+='(' + phone.charAt(2)+phone.charAt(3) + phone.charAt(4)+ ') ';
   }
+  
   if(phone.charAt(i) == '6'){
-    routeCode = phone.charAt(5) +
-                phone.charAt(5) +
-                phone.charAt(6) + '-'
+                newPhone+=phone.charAt(5) +phone.charAt(6) + phone.charAt(7) + '-';
   }
   if(phone.charAt(i) == '8'){
-  	numberCode = phone.charAt(7)+
-    					   phone.charAt(8)+ '-'+
-                           phone.charAt(9)+
-                           phone.charAt(10)
-  }
+                           newPhone+=phone.charAt(8)+
+                           phone.charAt(9)+ '-'+
+                           phone.charAt(10)+
+                           phone.charAt(11);
+ }
   
 
 
 }
-formatedPhone = countryCode + cityCode  + routeCode+ numberCode;
-return formatedPhone;
+
+return newPhone;
 }
 
 console.log(formattedPhone('+71234567890'));
-</script>
+
