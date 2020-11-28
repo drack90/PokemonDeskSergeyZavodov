b8086a0177ae867413c3a02bc85ef355bbf405f2
Sergey Zavodov <drack702@gmail.com>
2020-11-28 21:49:19 +0500

    hommework-6
    Сделал рандомный выбор покемона. Отрисовку кнопок.
    Продолжение домашнего задание смотреть "https://www.notion.so/HomeWork-8-1ee6fa00b7794ca294225efa3e99dfd8"

--- jsClass/pokemon.js
+++ jsClass/pokemon.js
@@ -1,28 +1,30 @@
-import {$AllBtn} from "../jsModule/buttons.js";
 
 class Selectors {
   constructor(name) {
     this.elHP = document.getElementById(`health-${name}`);
     this.elProgressBar = document.getElementById(`progressbar-${name}`);
+    this.elImg = document.getElementById(`img-${name}`)
   }
 }
 
 class Pokemon extends Selectors{
 
-  constructor({name, hp, type, selectors},) {
+  constructor({name, hp, type, selectors, img, attacks =[]},) {
     super(selectors);
     this.name = name;
-    this.defaultHP = hp;
+    this.hp = hp;
     this.damageHP = hp;
+    this.img = img;
     this.type = type;
-
+    this.attacks = attacks;
     this.renderHP();
+    this.renderImg();
   }
 
    changeHP = (count, cb) => {
     this.damageHP -= count;
-
-      if(this.damageHP <= 0){
+    let $AllBtn = document.getElementsByClassName('button');
+     if(this.damageHP <= 0){
         this.damageHP =0;
         alert('Бедный ' + this.name + ' проиграл бой!');
         //off all button which contain .button class
@@ -32,7 +34,7 @@ class Pokemon extends Selectors{
       }
 
     this.renderHP();
-    cb && cb();
+    cb && cb(count);
   }
 
    renderHP = () => {
@@ -41,12 +43,21 @@ class Pokemon extends Selectors{
 
   }
 
+  // function renderImg (player1){
+//   let $elImg = document.getElementById('img-player1');
+//   $elImg.src = player1.img;
+// }
+// renderImg(player1);
+  renderImg = () => {
+    this.elImg.src = this.img;
+  };
+
    renderHpLife = () => {
-    this.elHP.innerText = this.damageHP + ' / ' + this.defaultHP;
+    this.elHP.innerText = this.damageHP + ' / ' + this.hp;
   }
 
    renderProgressbarHP = () => {
-    let persentKick = this.damageHP/this.defaultHP*100;
+    let persentKick = this.damageHP/this.hp*100;
 
     if (Math.ceil(persentKick) < 60 && Math.ceil(persentKick) > 30 ){
       this.elProgressBar.classList.add('low')
@@ -56,6 +67,9 @@ class Pokemon extends Selectors{
 
     this.elProgressBar.style.width = Math.ceil(persentKick)  + '%'
   }
+
 }
+
+
 
 export default Pokemon;
--- main.js
+++ main.js
@@ -1,45 +1,81 @@
 import Pokemon from "./jsClass/pokemon.js";
-import { $btn, $btnShock, $AllBtn} from "./jsModule/buttons.js"
+import {pokemons} from "./jsModule/pokemons.js";
 import random from "./jsModule/util.js";
 import collectLog from "./jsModule/log.js";
 import countBtn from "./jsModule/buttonCounter.js";
+import {$modal} from "./includes/modal.js"
 
+
+// let dispPoke = [];
+// pokemons.forEach(item =>{dispPoke.push(item.img)});
+// console.log(dispPoke)
+// let sumPoke = dispPoke.length;
+// console.log(sumPoke)
+// let getPoke = () => {
+//   for(let i=0; i<sumPoke; i++){
+//    getPoke = dispPoke[i];
+//   }
+// }
+// let getp;
+//
+// let modal = $modal({
+//   class: 'log',
+//   tittle: 'Выберите своего бойца',
+//   content: `<img src="${getPoke()}"></img>`,
+//   footerButton:[
+//     { class: 'btn btn__cancel', text: 'Отмена', handler: 'modalHandlerCancel' },
+//     { class: 'btn btn__ok', text: 'ОК', handler: 'modalHandlerOk' }
+//   ]
+// });
+//
+//
+// modal.show();
+
+console.log(pokemons[0])
+const playerOne = pokemons[random(pokemons.length) -1];
 
 const player1 = new Pokemon({
-  name: 'Pickachu',
-  type: 'electrik',
-  hp:300,
-  selectors: 'character'
+  selectors: 'player1',
+  ...playerOne,
+
 });
+
+let pokeImg = player1.img;
+
+// function renderImg (player1){
+//   let $elImg = document.getElementById('img-player1');
+//   $elImg.src = player1.img;
+// }
+// renderImg(player1);
+
+
+console.log(player1)
+const $control = document.querySelector('.control')
+
+player1.attacks.forEach(item =>{
+  const $btn = document.createElement('button');
+  $btn.classList.add('button');
+  $btn.innerText = item.name;
+  const btnCount = countBtn(item.maxCount, $btn)
+  $btn.addEventListener('click', () =>{
+    btnCount();
+    player2.changeHP(random(item.maxDamage, item.minDamage), function (count) {
+      console.log(count)
+      collectLog(player2, player1, count);
+    });``
+  })
+  $control.appendChild($btn);
+
+})
 
 const player2 = new Pokemon({
   name: 'Charmander',
   type: 'fire',
   hp: 300,
-  selectors: 'enemy'
+  selectors: 'player2'
 });
 
-//add count and first init func
-const btnCountJolt = countBtn(6, $btn);
-//action on click
-$btn.addEventListener('click', function () {
-  btnCountJolt();
-  player1.changeHP(random(30, 5),  function (count) {
-    collectLog(player1, player2, count);
-  });
-  player2.changeHP(random(250, 200), function (count) {
-  collectLog(player2, player1, count);
-  });
-})
 
-
-const btnCountShok = countBtn(5, $btnShock);
-$btnShock.addEventListener('click',function () {
-  btnCountShok();
-  player2.changeHP(random(50, 10), function (count) {
-   collectLog(player2, player1, count);
-  });
-})
 
 
 
